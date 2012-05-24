fd51e98dfa385516daa4105491309c171c5f83e9
tkellogg <timothy.kellogg@gmail.com>
2012-05-23 20:35:11 -0600

    Added test project

---
+++ Moq.AutoMock.Tests/AutoMockerTests.cs
@@ -0,0 +1,27 @@
+﻿using Should;
+using Xunit;
+
+namespace Moq.AutoMock.Tests
+{
+    public class AutoMockerTests
+    {
+        #region Types used for testing
+
+        private class Empty
+        {
+        }
+
+        #endregion
+
+        public class DescribeGetInstance
+        {
+            [Fact]
+            public void It_creates_object_with_no_constructor()
+            {
+                var mocker = new AutoMocker();
+                var instance = mocker.GetInstance<Empty>();
+                instance.ShouldNotBeNull();
+            }
+        }
+    }
+}
--- Moq.AutoMock/AutoMocker.cs
+++ Moq.AutoMock/AutoMocker.cs
@@ -1,6 +1,13 @@
-﻿namespace Moq.AutoMock
+﻿using System;
+
+namespace Moq.AutoMock
 {
     public class AutoMocker
     {
+        public T GetInstance<T>()
+            where T : class
+        {
+            return Activator.CreateInstance<T>();
+        }
     }
 }
