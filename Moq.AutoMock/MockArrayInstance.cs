b433a4e3dfd515902255f9caa0ecfadc13d19909
Adam Hewitt <adamhewitt@olivetree.com>
2018-06-02 16:41:44 -0700

    Split apart Instance files

---
+++ Moq.AutoMock/MockArrayInstance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Moq.AutoMock
+{
+    internal sealed class MockArrayInstance : IInstance
+    {
+        private readonly Type _type;
+        private readonly List<IInstance> _mocks = new List<IInstance>();
+
+        public MockArrayInstance(Type type) => _type = type;
+
+        public object Value
+        {
+            get
+            {
+                int i = 0;
+                Array array = Array.CreateInstance(_type, _mocks.Count);
+                foreach (IInstance instance in _mocks)
+                    array.SetValue(instance.Value, i++); 
+                return array;
+            }
+        }
+
+        public bool IsMock { get { return _mocks.Any(m => m.IsMock); } }
+
+        public void Add(IInstance instance)
+        {
+            _mocks.Add(instance);
+        }
+    }
+}
---
+++ Moq.AutoMock/MockInstance.cs
@@ -0,0 +1,11 @@
+﻿namespace Moq.AutoMock
+{
+    internal sealed class MockInstance : IInstance
+    {
+        public MockInstance(Mock value) => Mock = value;
+
+        public object Value => Mock.Object;
+        public Mock Mock { get; }
+        public bool IsMock => true;
+    }
+}
