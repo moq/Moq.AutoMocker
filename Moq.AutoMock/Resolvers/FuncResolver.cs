356b4aa1223c4bef24a9e310db48cff7e75194b9
Adam Hewitt <adamhewitt@olivetree.com>
2018-06-06 15:08:19 -0700

    Center on .Get<T> for funcs, no need for additional hoops

--- Moq.AutoMock/Resolvers/FuncResolver.cs
+++ Moq.AutoMock/Resolvers/FuncResolver.cs
@@ -18,13 +18,15 @@ namespace Moq.AutoMock.Resolvers
             if (!stInfo.IsGenericType || !(serviceType.GetGenericTypeDefinition() is Type td)) return;
             if (td.Namespace != nameof(System) || !Regex.IsMatch(td.Name, $"^{nameof(Func<object>)}\\b")) return;
 
-            var invoke = serviceType.GetMethod("Invoke");
-            var returnType = invoke?.ReturnType;
+            var genericArgs = serviceType.GetGenericArguments();
+            var @params = genericArgs.Take(genericArgs.Length - 1)
+                .Select(t => Expression.Parameter(t));
+            var returnType = genericArgs.Last();
+
             if (returnType == null || returnType == typeof(void))
                 return; //I believe Moq will have created a value, we're really about giving it one that returns something from AM
 
-            Expression call = Expression.Call(typeof(FuncResolver), nameof(GetService), null,
-                Expression.Constant(am, typeof(AutoMocker)), Expression.Constant(returnType, typeof(Type)));
+            Expression call = Expression.Call(Expression.Constant(am), nameof(AutoMocker.Get), null, Expression.Constant(returnType, typeof(Type)));
 
             var rti = returnType.GetTypeInfo();
             if (rti.IsValueType || rti.IsPrimitive)
@@ -32,20 +34,7 @@ namespace Moq.AutoMock.Resolvers
             else
                 call = Expression.TypeAs(call, returnType);
 
-            var genericArgs = serviceType.GetGenericArguments();
-            var @params = genericArgs.Take(genericArgs.Length - 1).Select(t => Expression.Parameter(t));
-
             context.Value = Expression.Lambda(serviceType, call, @params).Compile();
-        }
-
-        private static object GetService(AutoMocker autoMocker, Type requestType)
-        {
-            //TODO - what does this really need to be?
-            //
-            //var methodName = true /*rti.IsInterface || rti.IsAbstract*/
-            //    ? nameof(AutoMocker.Get)
-            //    : nameof(AutoMocker.CreateInstance);    //TODO - needs tested
-            return autoMocker.Get(requestType);
         }
     }
 }
