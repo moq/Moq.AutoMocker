a8a7755a5ac55cf1cffea27892b720898d89bc3e
Adam Hewitt <adamhewitt@olivetree.com>
2018-06-05 22:00:01 -0700

    Implement delegate resolver

---
+++ Moq.AutoMock.Tests/ResolvesDelegates.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq.AutoMock.Resolvers;
+using System;
+using System.Collections.Generic;
+
+namespace Moq.AutoMock.Tests
+{
+    [TestClass]
+    public class ResolvesDelegates
+    {
+        delegate string ZeroParamDelegate();
+        delegate object OneParamDelegate(int p);
+        delegate long TwoParamDelegate(int p, object foo);
+
+        [DataTestMethod, DynamicData(nameof(Delegates))]
+        public void ResolvesFuncReturningDefinedParameter(Type delegateType, object expected)
+        {
+            var mocker = new AutoMocker { Resolvers = { new DelegateResolver() } };
+            mocker.Use(expected.GetType(), expected);
+
+            var func = (Delegate)mocker.Get(delegateType);
+            Assert.IsNotNull(func);
+
+            var @params = new object[delegateType.GetMethod("Invoke").GetParameters().Length];
+            Assert.AreEqual(expected, func.DynamicInvoke(@params));
+        }
+        static IEnumerable<object[]> Delegates
+        {
+            get
+            {
+                yield return new[] { typeof(Func<object>), new object() };
+                yield return new[] { typeof(Func<string, object>), new object() };
+                yield return new object[] { typeof(Func<object, string>), nameof(ResolvesDelegates) };
+                yield return new object[] { typeof(Func<object, ResolvesDelegates, string>), nameof(ResolvesDelegates) };
+                yield return new object[] { typeof(Func<string, int, Service2>), new Service2() };
+                yield return new object[] { typeof(ZeroParamDelegate), nameof(Service2) };
+                yield return new object[] { typeof(OneParamDelegate), new object() };
+                yield return new object[] { typeof(TwoParamDelegate), 42L };
+            }
+        }
+    }
+}
---
+++ Moq.AutoMock/Resolvers/DelegateResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Moq.AutoMock.Resolvers
+{
+    public class DelegateResolver : IMockResolver
+    {
+        public void Resolve(MockResolutionContext context)
+        {
+            if (context == null) return;
+
+            var (am, serviceType, value) = context;
+            if (!typeof(Delegate).GetTypeInfo().IsAssignableFrom(serviceType.GetTypeInfo()))
+                return; //We aren't a delegate
+
+            var invoke = serviceType.GetMethod("Invoke");
+            var returnType = invoke?.ReturnType;
+            if (returnType == null || returnType == typeof(void))
+                return; //I believe Moq will have created a value, we're really about giving it one that returns something from AM
+
+            //TODO - what does this really need to be?
+            var rti = returnType.GetTypeInfo();
+            var methodName = true /*rti.IsInterface || rti.IsAbstract*/
+                ? nameof(AutoMocker.Get)
+                : nameof(AutoMocker.CreateInstance);    //TODO - needs tested
+
+            Expression call = Expression.Call(Expression.Constant(am, typeof(AutoMocker)), methodName, null, Expression.Constant(returnType, typeof(Type)));
+            if (rti.IsValueType || rti.IsPrimitive)
+                call = Expression.Unbox(call, returnType);
+            else
+                call = Expression.TypeAs(call, returnType);
+
+            var @params = invoke.GetParameters().Select(pi => Expression.Parameter(pi.ParameterType));
+            var lambda = Expression.Lambda(serviceType, call, @params)
+                .Compile();
+
+            context.Value = lambda;
+        }
+    }
+}
