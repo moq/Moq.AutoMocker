6bd20a932b00c58f6688ac905b358434638a6c0f
Simon Cropp <simon.cropp@gmail.com>
2020-09-24 20:26:20 +1000

    Update DescribeExtractingStrictObjects.cs

--- Moq.AutoMock.Tests/DescribeExtractingStrictObjects.cs
+++ Moq.AutoMock.Tests/DescribeExtractingStrictObjects.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq.AutoMock.Tests.Util;
+using VerifyMSTest;
 
 namespace Moq.AutoMock.Tests
 {
     [TestClass]
-    public class DescribeExtractingStrictObjects
+    public class DescribeExtractingStrictObjects : VerifyBase
     {
         [TestMethod]
         public void It_creates_a_mock_as_strict_if_the_object_is_missing()
