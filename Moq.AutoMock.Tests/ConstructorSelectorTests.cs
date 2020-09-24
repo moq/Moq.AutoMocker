38605a213dd3ddfdd7a6668c878522607da1f82f
Simon Cropp <simon.cropp@gmail.com>
2020-09-24 20:27:23 +1000

    Update ConstructorSelectorTests.cs

--- Moq.AutoMock.Tests/ConstructorSelectorTests.cs
+++ Moq.AutoMock.Tests/ConstructorSelectorTests.cs
@@ -1,56 +1,58 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Reflection;
+using System.Threading.Tasks;
 using Moq.AutoMock.Tests.Util;
+using VerifyMSTest;
 
 namespace Moq.AutoMock.Tests
 {
     [TestClass]
-    public class ConstructorSelectorTests
+    public class ConstructorSelectorTests : VerifyBase
     {
         private const BindingFlags DefaultBindingFlags = BindingFlags.Instance | BindingFlags.Public;
 
         [TestMethod]
-        public void It_chooses_the_ctor_with_arguments()
+        public Task It_chooses_the_ctor_with_arguments()
         {
             var ctor = typeof(WithDefaultAndSingleParameter).SelectCtor(Array.Empty<Type>(), DefaultBindingFlags);
-            Assert.AreEqual(1, ctor.GetParameters().Length);
+            return Verify(ctor);
         }
 
         [TestMethod]
-        public void It_chooses_the_ctor_with_the_most_arguments()
+        public Task It_chooses_the_ctor_with_the_most_arguments()
         {
             var ctor = typeof(With3Parameters).SelectCtor(Array.Empty<Type>(), DefaultBindingFlags);
-            Assert.AreEqual(2, ctor.GetParameters().Length);
+            return Verify(ctor);
         }
 
         [TestMethod]
-        public void It_chooses_the_ctor_with_the_most_arguments_when_arguments_are_arrays()
+        public Task It_chooses_the_ctor_with_the_most_arguments_when_arguments_are_arrays()
         {
             var ctor = typeof(WithArrayParameter).SelectCtor(Array.Empty<Type>(), DefaultBindingFlags);
-            Assert.AreEqual(1, ctor.GetParameters().Length);
+            return Verify(ctor);
         }
 
         [TestMethod]
-        public void It_wont_select_if_an_argument_is_sealed_and_not_array()
+        public Task It_wont_select_if_an_argument_is_sealed_and_not_array()
         {
             var ctor = typeof(WithSealedParameter).SelectCtor(Array.Empty<Type>(), DefaultBindingFlags);
-            Assert.AreEqual(0, ctor.GetParameters().Length);
+            return Verify(ctor);
         }
 
         [TestMethod]
-        public void It_will_select_if_an_argument_is_sealed_and_supplied()
+        public Task It_will_select_if_an_argument_is_sealed_and_supplied()
         {
             var ctor = typeof(WithSealedParameter).SelectCtor(new [] { typeof(string) }, DefaultBindingFlags);
-            Assert.AreEqual(1, ctor.GetParameters().Length);
+            return Verify(ctor);
         }
 
         [TestMethod]
-        public void It_will_select_a_private_ctor_when_specified()
+        public Task It_will_select_a_private_ctor_when_specified()
         {
             const BindingFlags privateBindingFlags = DefaultBindingFlags | BindingFlags.NonPublic;
             var ctor = typeof(WithPrivateConstructor).SelectCtor(Array.Empty<Type>(), privateBindingFlags);
-            Assert.AreEqual(2, ctor.GetParameters().Length);
+            return Verify(ctor);
         }
     }
 }
