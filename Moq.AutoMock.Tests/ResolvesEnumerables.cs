d0e4139aca892292b74ea22e92dd69510b3dbd35
Daniel Weber <dw@exram.de>
2020-11-19 11:56:01 +0100

    Support resolution of IEnumerable<T>.

---
+++ Moq.AutoMock.Tests/ResolvesEnumerables.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq.AutoMock.Resolvers;
+
+namespace Moq.AutoMock.Tests
+{
+    [TestClass]
+    public class ResolvesEnumerables
+    {
+        [TestMethod]
+        public void ResolvesObjectEnumerableFromContainer() => Resolves(new object());
+
+        [TestMethod]
+        public void ResolvesNumberEnumerableFromContainer() => Resolves(42L);
+
+        [TestMethod]
+        public void ResolvesStringEnumerableFromContainer() => Resolves(nameof(ResolvesEnumerables));
+
+        private static void Resolves<T>(T expected)
+        {
+            var mocker = new AutoMocker { Resolvers = { new EnumerableResolver() } };
+            mocker.Use(expected!);
+
+            var enumerable = mocker.Get<IEnumerable<T>>();
+            Assert.IsNotNull(enumerable);
+            CollectionAssert.AreEquivalent(enumerable.ToArray(), new[] { expected });
+        }
+    }
+}
---
+++ Moq.AutoMock/Resolvers/EnumerableResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Moq.AutoMock.Resolvers
+{
+    /// <summary>
+    /// A resolver that resolves IEnumerable&lt;T&gt; requested types.
+    /// </summary>
+    public class EnumerableResolver : IMockResolver
+    {
+        /// <summary>
+        /// Resolves IEnumerable&lt;T&gt; types.
+        /// </summary>
+        /// <param name="context">The resolution context.</param>
+        public void Resolve(MockResolutionContext context)
+        {
+            var (am, serviceType, _) = context ?? throw new ArgumentNullException(nameof(context));
+
+            if (!serviceType.GetTypeInfo().IsGenericType || serviceType.GetGenericTypeDefinition() != typeof(IEnumerable<>))
+                return;
+
+            var elementType = serviceType.GetGenericArguments().Single();
+            var array = System.Array.CreateInstance(elementType, 1);
+
+            array.SetValue(am.Get(elementType), 0);
+            context.Value = array;
+        }
+    }
+}
