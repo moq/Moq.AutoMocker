bec09f8a79c8af449d7b8bb01d6512ad4b7ca4fb
Adam Hewitt <adamhewitt627@users.noreply.github.com>
2020-11-22 22:46:02 -0800

    Resolve some suppressed warnings. (#95)
    * Resolve some suppressed warnings
    
    * Fix tests
    
    * Revert the unused attribute code

--- Moq.AutoMock.Tests/Util/WithSealedParameter.cs
+++ Moq.AutoMock.Tests/Util/WithSealedParameter.cs
@@ -2,15 +2,7 @@
 {
     public class WithSealedParameter
     {
-        // ReSharper disable UnusedMember.Global
-        // ReSharper disable UnusedParameter.Local
         public WithSealedParameter() { }
-
-#pragma warning disable CA1801  //Parameter  is never used. Remove the parameter or use it in the method body
-        public WithSealedParameter(string @sealed) { }
-#pragma warning restore CA1801  //Parameter  is never used. Remove the parameter or use it in the method body
-
-        // ReSharper restore UnusedParameter.Local
-        // ReSharper restore UnusedMember.Global
+        public WithSealedParameter(string _) { }
     }
 }
