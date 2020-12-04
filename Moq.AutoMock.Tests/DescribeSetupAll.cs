607ecc3a504fa8545965638f7611ac66f4220ebb
Kevin Bost <kitokeboo@gmail.com>
2020-12-03 23:01:33 -0800

    WIP setting up an SetupAll method

---
+++ Moq.AutoMock.Tests/DescribeSetupAll.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq.AutoMock.Tests.Util;
+using System;
+
+namespace Moq.AutoMock.Tests
+{
+    [TestClass]
+    public class DescribeSetupAll
+    {
+        [TestMethod]
+        public void You_can_setup_all_on_a_method_with_a_return_value()
+        {
+            string expected = "SomeValue";
+            Mock<IService4> mock = new();
+
+            mock.SetupAll<IService4, string>(nameof(IService4.MainMethodName))
+                .Returns(expected);
+
+            string result = mock.Object.MainMethodName("Something");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void You_can_setup_all_on_a_void_method()
+        {
+            Mock<IService6> mock = new();
+
+            mock.SetupAll(nameof(IService6.Void))
+                .Verifiable();
+            
+            mock.Object.Void(42, "SomeValue");
+
+            mock.VerifyAll();
+        }
+    }
+}
---
+++ Moq.AutoMock/MockExtensions.cs
@@ -0,0 +1,84 @@
+﻿using Moq.Language.Flow;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Moq.AutoMock
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public static class MockExtensions
+    {
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mock"></param>
+        /// <param name="methodName"></param>
+        /// <returns></returns>
+        public static ISetup<T> SetupAll<T>(this Mock<T> mock, string methodName)
+            where T : class
+        {
+            if (mock is null)
+            {
+                throw new ArgumentNullException(nameof(mock));
+            }
+
+            LambdaExpression lambdaExpression = GetExpression<T>(methodName);
+
+            MethodInfo setupMethod = mock.GetType().GetMethods()
+                .Single(x => x.Name == nameof(Mock<object>.Setup) && x.ReturnType.GetGenericArguments().Length == 1);
+            return (ISetup<T>)setupMethod.Invoke(mock, new object[] { lambdaExpression })!;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="mock"></param>
+        /// <param name="methodName"></param>
+        /// <returns></returns>
+        public static ISetup<T, TResult> SetupAll<T, TResult>(this Mock<T> mock, string methodName)
+            where T : class
+        {
+            if (mock is null)
+            {
+                throw new ArgumentNullException(nameof(mock));
+            }
+
+            LambdaExpression lambdaExpression = GetExpression<T>(methodName);
+
+            //Invoke the setup method
+            MethodInfo setupMethod = mock.GetType().GetMethods()
+                .Single(x => x.Name == nameof(Mock<object>.Setup) && x.ReturnType.GetGenericArguments().Length == 2);
+            var ret = setupMethod.MakeGenericMethod(typeof(TResult)).Invoke(mock, new object[] { lambdaExpression });
+            return (ISetup<T, TResult>)ret!;
+        }
+
+        private static LambdaExpression GetExpression<T>(string methodName)
+        {
+            //Build up the expression to pass to the Setup method
+            MethodInfo method = typeof(T).GetMethod(methodName)!;
+            if (method is null) throw new Exception("Boom");
+
+            var itType = typeof(Moq.It);
+            var isAnyMethod = itType.GetMethod(nameof(It.IsAny));
+
+            var parameterExpressions = method.GetParameters()
+                .Select(parameter =>
+                {
+                    var closeAnyMethod = isAnyMethod!.MakeGenericMethod(parameter.ParameterType);
+                    return Expression.Call(null, closeAnyMethod);
+                })
+                .ToArray();
+
+            var xExpression = Expression.Parameter(typeof(T), "x");
+            var methodCallExpression = Expression.Call(xExpression, method, parameterExpressions);
+            return Expression.Lambda(methodCallExpression, false, new[] { xExpression });
+        }
+    }
+
+}
