5e3260f470c6877ff5413b33003d330b5caa05f6
Kevin Bost <kitokeboo@gmail.com>
2021-04-08 08:39:59 -0700

    WIP

--- Moq.AutoMock.Tests/DescribeCombiningTypes.cs
+++ Moq.AutoMock.Tests/DescribeCombiningTypes.cs
@@ -17,14 +17,34 @@ namespace Moq.AutoMock.Tests
             Assert.AreSame(mocker.Get<IService3>(), mocker.Get<IService2>());
         }
 
-        [TestMethod]
-        public void Convenience_methods_work()
-        {
-            var mocker = new AutoMocker();
-            mocker.Combine<IService1, IService2, IService3>();
+        //[TestMethod]
+        //public void Convenience_methods_work()
+        //{
+        //    var mocker = new AutoMocker();
+        //    mocker.Combine<IService1, IService2, IService3>();
+        //
+        //    Assert.AreSame(mocker.Get<IService2>(), mocker.Get<IService1>());
+        //    Assert.AreSame(mocker.Get<IService3>(), mocker.Get<IService2>());
+        //}
 
-            Assert.AreSame(mocker.Get<IService2>(), mocker.Get<IService1>());
-            Assert.AreSame(mocker.Get<IService3>(), mocker.Get<IService2>());
+        [TestMethod]
+        public void Combine_method_should_maintain_setups()
+        {
+            var mocker2 = new AutoMocker(MockBehavior.Loose);
+            mocker2.GetMock<IDerivedInterface>().Setup(x => x.Foo()).Returns(() => "42");
+            mocker2.Combine(typeof(IDerivedInterface), typeof(IBaseInterface));
+
+            Assert.Equals("42", mocker2.Get<IBaseInterface>().Foo());
+            Assert.Equals("42", mocker2.Get<IDerivedInterface>().Foo());
+        }
+
+        interface IBaseInterface
+        {
+            string Foo();
+        }
+        interface IDerivedInterface : IBaseInterface
+        {
+            string Bar();
         }
     }
 }
