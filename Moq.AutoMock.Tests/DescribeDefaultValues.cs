45d5ef50da3def35c841479c99aadd78afc34b93
Kevin B <Keboo@users.noreply.github.com>
2022-05-02 22:12:21 -0700

    Adding support for DefaultValueProvider (#145)
    This exposes the DefaultValueProvider property from Mock<T> through AutoMocker

---
+++ Moq.AutoMock.Tests/DescribeDefaultValues.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq.AutoMock.Tests.Util;
+
+namespace Moq.AutoMock.Tests;
+
+[TestClass]
+public class DescribeDefaultValues
+{
+    [TestMethod]
+    [DataRow(DefaultValue.Mock)]
+    [DataRow(DefaultValue.Empty)]
+    [Description("Issue 144")]
+    public void It_creates_mocks_with_default_value_set(DefaultValue defaultValue)
+    {
+        AutoMocker mocker = new(MockBehavior.Default, defaultValue);
+
+        Mock<IService1> mock = mocker.GetMock<IService1>();
+
+        Assert.AreEqual(defaultValue, mock.DefaultValue);
+    }
+
+    [TestMethod]
+    [Description("Issue 144")]
+    public void It_creates_mocks_with_custom_default_value_resolver()
+    {
+        CustomDefaultValueProvider defaultValueProvider = new(); 
+        AutoMocker mocker = new(MockBehavior.Default, DefaultValue.Custom, defaultValueProvider, false);
+
+        Mock<IService1> mock = mocker.GetMock<IService1>();
+
+        Assert.AreEqual(DefaultValue.Custom, mock.DefaultValue);
+        Assert.AreEqual(defaultValueProvider, mock.DefaultValueProvider);
+    }
+
+    private class CustomDefaultValueProvider : DefaultValueProvider
+    {
+        protected override object GetDefaultValue(Type type, Mock mock)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Moq.AutoMock.Tests/DescribeWithSelfMock.cs
@@ -0,0 +1,102 @@
+﻿namespace Moq.AutoMock.Tests;
+
+[TestClass]
+public class DescribeWithSelfMock
+{
+    [TestMethod]
+    [Description("Issue 144")]
+    public void It_can_register_a_custom_default_value_provider_for_a_self_mock_with_interface()
+    {
+        AutoMocker mocker = new();
+        CustomDefaultValueProvider provider = new();
+
+        var mock = mocker.WithSelfMock<IService2, Service2>(defaultValue: DefaultValue.Custom, defaultValueProvider: provider);
+
+        Assert.AreEqual(provider, Mock.Get(mock).DefaultValueProvider);
+    }
+
+    [TestMethod]
+    [Description("Issue 144")]
+    public void It_uses_default_value_provider_for_a_self_mock_with_interface()
+    {
+        CustomDefaultValueProvider provider = new();
+        AutoMocker mocker = new(MockBehavior.Default, DefaultValue.Custom, provider, false);
+
+        var mock = mocker.WithSelfMock<IService2, Service2>();
+
+        Assert.AreEqual(provider, Mock.Get(mock).DefaultValueProvider);
+    }
+
+    [TestMethod]
+    [Description("Issue 144")]
+    public void It_can_register_a_custom_default_value_provider_for_a_self_mock_with_class()
+    {
+        AutoMocker mocker = new();
+        CustomDefaultValueProvider provider = new();
+
+        var mock = mocker.WithSelfMock<Service2>(defaultValue: DefaultValue.Custom, defaultValueProvider: provider);
+
+        Assert.AreEqual(provider, Mock.Get(mock).DefaultValueProvider);
+    }
+
+    [TestMethod]
+    [Description("Issue 144")]
+    public void It_uses_default_value_provider_for_a_self_mock_with_class()
+    {
+        CustomDefaultValueProvider provider = new();
+        AutoMocker mocker = new(MockBehavior.Default, DefaultValue.Custom, provider, false);
+
+        var mock = mocker.WithSelfMock<Service2>();
+
+        Assert.AreEqual(provider, Mock.Get(mock).DefaultValueProvider);
+    }
+
+    [TestMethod]
+    [Description("Issue 144")]
+    public void It_can_register_a_custom_default_value_provider_for_a_self_mock_with_interface_type()
+    {
+        AutoMocker mocker = new();
+        CustomDefaultValueProvider provider = new();
+
+        var mock = (Service2)mocker.WithSelfMock(typeof(IService2), typeof(Service2), defaultValue: DefaultValue.Custom, defaultValueProvider: provider);
+
+        Assert.AreEqual(provider, Mock.Get(mock).DefaultValueProvider);
+    }
+
+    [TestMethod]
+    [Description("Issue 144")]
+    public void It_uses_default_value_provider_for_a_self_mock_with_interface_type()
+    {
+        CustomDefaultValueProvider provider = new();
+        AutoMocker mocker = new(MockBehavior.Default, DefaultValue.Custom, provider, false);
+
+        var mock = (Service2)mocker.WithSelfMock(typeof(IService2), typeof(Service2));
+
+        Assert.AreEqual(provider, Mock.Get(mock).DefaultValueProvider);
+    }
+
+    [TestMethod]
+    [Description("Issue 144")]
+    public void It_can_register_a_custom_default_value_provider_for_a_self_mock_with_class_type()
+    {
+        AutoMocker mocker = new();
+        CustomDefaultValueProvider provider = new();
+
+        var mock = (Service2)mocker.WithSelfMock(typeof(Service2), defaultValue: DefaultValue.Custom, defaultValueProvider: provider);
+
+        Assert.AreEqual(provider, Mock.Get(mock).DefaultValueProvider);
+    }
+
+    [TestMethod]
+    [Description("Issue 144")]
+    public void It_uses_default_value_provider_for_a_self_mock_with_class_type()
+    {
+        CustomDefaultValueProvider provider = new();
+        AutoMocker mocker = new(MockBehavior.Default, DefaultValue.Custom, provider, false);
+
+        var mock = (Service2)mocker.WithSelfMock(typeof(Service2));
+
+        Assert.AreEqual(provider, Mock.Get(mock).DefaultValueProvider);
+    }
+}
+
