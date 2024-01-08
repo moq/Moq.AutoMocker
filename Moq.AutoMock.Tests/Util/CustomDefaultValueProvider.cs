09ba0b505c4ec47b6a59bac8d014f08fb9f3e1a8
Kevin B <Keboo@users.noreply.github.com>
2024-01-08 09:16:35 -0800

    Some code clean up (#243)
    * Some code clean up
    
    * Enabled centralized package management
    
    Updated NuGets
    Fixed issue with NUnit test generator.
    
    * Role back compiler NuGets
    
    Also disabling dependabot for them. We need the old version for net6

--- Moq.AutoMock.Tests/Util/CustomDefaultValueProvider.cs
+++ Moq.AutoMock.Tests/Util/CustomDefaultValueProvider.cs
@@ -1,14 +1,14 @@
 ﻿namespace Moq.AutoMock.Tests.Util;
 internal class CustomDefaultValueProvider : DefaultValueProvider
 {
-    public Dictionary<Type, object?> DefaultValues { get; } = new();
+    public Dictionary<Type, object> DefaultValues { get; } = new();
 
-    protected override object? GetDefaultValue(Type type, Mock mock)
+    protected override object GetDefaultValue(Type type, Mock mock)
     {
         if (DefaultValues.TryGetValue(type, out object? value))
         {
             return value;
         }
-        return null;
+        throw new NotImplementedException();
     }
 }
