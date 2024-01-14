c259f2af1bf20c3bb4e62bbc4edf571ff656090f
Kevin B <Keboo@users.noreply.github.com>
2024-01-14 07:40:10 -0800

    Adding test for primary constructors

---
+++ Moq.AutoMock.Tests/Util/PrimaryConstructor.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Moq.AutoMock.Tests.Util;
+
+[ExcludeFromCodeCoverage]
+public class WithService(IService1 service)
+{
+    public IService1 Service { get; set; } = service;
+}
