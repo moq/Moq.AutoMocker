03e1f480c5e6ac4bcd03df83a0a2259f91d88586
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-01-23 21:23:28 -0800

    Test generator

---
+++ GeneratorTests/Moq.AutoMock.Generator.Example.xUnit/ControllerStringTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moq.AutoMock.Generator.Example.xUnit;
+
+[ConstructorTests(TargetType = typeof(ControllerString))]
+public partial class ControllerStringTests
+{
+    partial void AutoMockerTestSetup(Moq.AutoMock.AutoMocker mocker, string testName)
+    {
+        mocker.Use<string>("");
+    }
+}
---
+++ GeneratorTests/Moq.AutoMocker.Generator.Example/ControllerString.cs
@@ -0,0 +1,13 @@
+﻿namespace Moq.AutoMock.Generator.Example;
+
+public class ControllerString
+{
+    public ControllerString(string name, string? testName = null)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        TestName = testName;
+    }
+
+    public string Name { get; } = "";
+    public string? TestName { get; }
+}
