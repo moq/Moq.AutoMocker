348e0d2875b07a14d7fea6f03410ee81fae487d2
Kevin Bost <kitokeboo@gmail.com>
2025-11-09 22:08:00 -0800

    Adds support for callback-based service resolution
    Introduces new `Use` overloads that accept factory delegates for registering services. These callbacks enable lazy instantiation and caching of service instances, invoked only when the service is first requested.
    
    A new `CallbackResolver` manages and executes these factory functions. This provides a flexible way to configure service creation with deferred execution and ensures that services created via callback are cached for subsequent requests.
    
    Fixes: #196

---
+++ Moq.AutoMock.Tests/DescribeUsingWithCallback.cs
@@ -0,0 +1,54 @@
+﻿namespace Moq.AutoMock.Tests;
+
+[TestClass]
+public class DescribeUsingWithCallback
+{
+    [TestMethod]
+    public void You_can_register_a_callback_to_configure_a_mock()
+    {
+        AutoMocker mocker = new();
+        mocker.Use<IService2>(mocker =>
+        {
+            return new Service2();
+        });
+        
+        var instance = mocker.Get<IService2>();
+        Assert.IsInstanceOfType(instance, typeof(Service2));
+    }
+
+    [TestMethod]
+    public void Service_callback_is_not_invoked_until_the_service_is_requested()
+    {
+        AutoMocker mocker = new();
+        bool callbackInvoked = false;
+        mocker.Use<IService2>(mocker =>
+        {
+            callbackInvoked = true;
+            return new Service2();
+        });
+
+        
+        bool beforeRequest = callbackInvoked;
+        _ = mocker.CreateInstance<WithService>();
+
+        Assert.IsFalse(beforeRequest, "Callback should not have been invoked yet.");
+        Assert.IsTrue(callbackInvoked, "Callback should have been invoked when the service was requested.");
+    }
+
+    [TestMethod]
+    public void Service_created_from_a_callback_is_cached()
+    {
+        AutoMocker mocker = new();
+        int callbackCount = 0;
+        mocker.Use<IService2>(() =>
+        {
+            callbackCount++;
+            return new Service2();
+        });
+
+        _ = mocker.CreateInstance<WithService>();
+        _ = mocker.CreateInstance<WithService>();
+        
+        Assert.AreEqual(1, callbackCount);
+    }
+}
